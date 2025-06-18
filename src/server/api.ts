841abffbe73c975c1eca51b95eb4a7557069ef7e
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2025-06-18 15:41:21 -0300

    Alterações de variaveis de ambiente

--- src/server/api.ts
+++ src/server/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
     //baseURL: "http://localhost:3333"
-    baseURL: "https://backcaldascard.vercel.app/"
+    baseURL: "https://backcaldascard.vercel.app"
     //baseURL: "https://backbeer.vercel.app/"
 })
